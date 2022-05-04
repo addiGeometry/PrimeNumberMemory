893b9e54a682d37a2b1bf11777b250d2d0e61079
addiGeometry <adrian@banas.de>
2022-05-04 11:33:42 +0200

    Implemented API, Modified the Design-Builder, First Custom exception. Struggles with String format
    Code not running as of rn

---
+++ src/PrimeNumberMemory.java
@@ -0,0 +1,18 @@
+public class PrimeNumberMemory {
+    public static void main(String args[]){
+        System.out.println("Welcome to Prime Number Memory version 1.0");
+
+        if(args.length < 1){
+            System.err.println("there needs to be a player for a game");
+            System.exit(1);
+        }
+
+        System.out.println("Greatings " + args[0]);
+        System.out.println("Enjoy the ride");
+
+        MemoryGui userCMD = new MemoryGui(args[0],System.out, System.in);
+
+        userCMD.printInstructions();
+        userCMD.doCommandLoop();
+    }
+}
